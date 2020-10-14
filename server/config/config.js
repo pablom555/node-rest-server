2e506fd6f0ba90868fd64bb647c7b5c61c7ad57b
Pablo Martinez <pablomartinez555@hotmail.com>
2020-10-14 15:59:14 -0300

    variable entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';    
 } else {
-    urlDB = 'mongodb+srv://pablom555:Latella11@dbcluster.jmmze.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_ATLAS_URI;
 }
 
 process.env.URLDB = urlDB; 
